fd5abcf3998045e1aa5370ba75f4f2abf3438df2
Ed Wilson <edwilsonfa@gmail.com>
2023-11-16 22:23:31 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -1,43 +1,25 @@
-package com.devsuperior.dslist.entities;
+package com.devsuperior.dslist.dto;
 
-import jakarta.persistence.*;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+import com.devsuperior.dslist.entities.Game;
+import jakarta.persistence.Column;
+import org.springframework.beans.BeanUtils;
 
-import java.io.Serializable;
-import java.util.Objects;
-import java.util.Optional;
-
-@Entity
-@Table(name = "tb_game")
-public class Game implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class GameDTO {
     private Long id;
     private String title;
-    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
-    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game() {
+    public GameDTO() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platforms = platforms;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+    public GameDTO(Game entity){
+        BeanUtils.copyProperties(entity, this);
     }
 
     public Long getId() {
@@ -76,7 +58,7 @@ public class Game implements Serializable {
         return platforms;
     }
 
-    public void setPlatform(String platforms) {
+    public void setPlatforms(String platforms) {
         this.platforms = platforms;
     }
 
@@ -111,19 +93,4 @@ public class Game implements Serializable {
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Game game = (Game) o;
-        return Objects.equals(id, game.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-
 }
