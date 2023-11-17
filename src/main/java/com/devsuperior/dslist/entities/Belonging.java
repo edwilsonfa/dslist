612d3ca91293e8c8af7e02f113ede457a95c4865
Ed Wilson <edwilsonfa@gmail.com>
2023-11-16 21:42:25 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPK id;
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+}
